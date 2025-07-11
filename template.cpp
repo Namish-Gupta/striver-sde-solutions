6db75d2bfffa061df61a5aa2c099bc5619ea992f
Namish <namishdev.gupta@gmail.com>
2025-07-11 22:28:27 +0530

    Day00: repo scaffold & template.cpp

---
+++ template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define ff first
+#define ss second
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // Your code here
+
+    return 0;
+}
